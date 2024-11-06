f0cc48fe27b186c6e83a2745f8d13c218b75b387
Elabs-llc <edwinasare08@gmail.com>
2024-11-05 23:26:06 -0800

    styles updated for front-end

--- skynexus/settings.py
+++ skynexus/settings.py
@@ -33,13 +33,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'skydata.apps.SkydataConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'skydata', # instead of 'skydata.apps.SkydataConfig',
 ]
 
 MIDDLEWARE = [

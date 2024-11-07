ba53221b2fe40b84b936ca6c01d5ce39c6d0bab1
Elabs-llc <edwinasare08@gmail.com>
2024-11-07 03:15:15 -0800

    Serializer class created

---
+++ skydata/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import SkynexusData
+
+# Serializers convert complex data types (like Django models) to 
+# Python primitives that can be rendered into JSON
+# ModelSerializer automatically creates a set of fields based on 
+# your model
+# fields list specifies which model fields to include in the 
+# serialization
+# read_only_fields indicates fields that can't be modified 
+# through the API
+class SkynexusDataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SkynexusData
+        fields = ['id', 'timestamp', 'value', 'description', 'created_at', 'updated_at']
+        read_only_fields = ('id', 'created_at')
